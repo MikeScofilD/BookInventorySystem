568fb80c902d1d72aa3546d3348cb8b0126eed22
Artem <38011337+MikeScofilD@users.noreply.github.com>
2023-06-15 17:58:34 +0300

    adde navigationGroup User

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -26,7 +26,8 @@ class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-collection';
+    protected static ?string $navigationIcon = 'heroicon-o-users';
+    protected static ?string $navigationGroup = 'Admin Management';
 
     public static function form(Form $form): Form
     {
