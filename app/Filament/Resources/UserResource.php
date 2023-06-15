86ce4a91991287250d91aaf4e19335a2081659ac
Artem <38011337+MikeScofilD@users.noreply.github.com>
2023-06-15 17:57:02 +0300

    created user resource

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use App\Models\User;
+use Filament\Tables;
+use Filament\Resources\Form;
+use Filament\Resources\Table;
+use Filament\Resources\Resource;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Actions\DeleteAction;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\Pages\CreateUser;
+use App\Filament\Resources\UserResource\Pages\EditUser;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Filament\Resources\UserResource\RelationManagers\RolesRelationManager;
+use Filament\Forms\Components\CheckboxList;
+use Filament\Pages\Page;
+use Filament\Tables\Filters\TrashedFilter;
+use Illuminate\Support\Facades\Hash;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Toggle::make('is_admin')
+                    ->required(),
+                Forms\Components\TextInput::make('email')
+                    ->email()
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('password')
+                    ->password()
+                    ->dehydrateStateUsing(static fn(null|string $state): null|string => filled($state) ? Hash::make($state):null)
+                    ->required(static fn(Page $livewire): string=> $livewire instanceof CreateUser)
+                    ->maxLength(255)
+                    ->dehydrated(static fn(null|string $state): bool =>
+                    filled($state),
+                    )->label(static fn(Page $livewire): string=>($livewire instanceof EditUser)? 'New Password' : 'Password'),
+                CheckboxList::make('roles')->relationship('roles', 'name')->columns(2)->helperText('Only Choose One!')->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\IconColumn::make('is_admin')->sortable()->searchable()
+                    ->boolean(),
+                TextColumn::make('roles.name')->sortable()->searchable(),
+                Tables\Columns\TextColumn::make('email')->sortable()->searchable(),
+                Tables\Columns\TextColumn::make('email_verified_at')
+                    ->dateTime(),
+                TextColumn::make('deleted_at')->dateTime('d-M-Y')->sortable()->searchable(),
+                TextColumn::make('created_at')->dateTime('d-M-Y')->sortable()->searchable()
+            ])
+            ->filters([
+                TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                DeleteAction::make()
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+           RolesRelationManager::class,
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
